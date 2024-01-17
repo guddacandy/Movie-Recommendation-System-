7224aa8a9c22c6d866052ed2a517e291cd1786bf
dev-ezzy <bettezra07@gmail.com>
2024-01-17 16:18:01 +0300

    com

--- recommender.py
+++ recommender.py
@@ -1,5 +1,8 @@
 #importing modules
 import pandas as pd
+import numpy as np
+from surprise import KNNBasic, accuracy
+
 
 # recommender function
 movies = pd.read_csv("data/movies.csv")
@@ -54,5 +57,35 @@ def neural_recommender(user_id, n, df, svd_model, neural_network_model):
 
     return top_recommendations[['movieId', 'title', 'genres', 'predicted_rating']]
 
-# Example usage:
 
+#KNN caller function
+def knn_caller(user_id, n, model):
+    user_id = user_id
+    N = n
+    # Get a list of all movie IDs in your dataset
+    all_movie_ids = np.unique(modelling_data['movieId'])
+
+    # Create a list to store predicted ratings for unrated movies
+    predicted_ratings = []
+
+    # Predict ratings for the user on unrated movies
+    for movie_id in all_movie_ids:
+        # Check if the user has already rated the movie
+        if not ratings[(ratings['userId'] == user_id) & (ratings['movieId'] == movie_id)].empty:
+            predicted_rating = model.predict(user_id, movie_id)
+            predicted_ratings.append((movie_id, predicted_rating.est))
+            #Sort the predicted ratings in descending order
+            predicted_ratings.sort(key=lambda x: x[1], reverse=True)
+
+            # Get the top 5 movie recommendations
+            top_5_recommendations = predicted_ratings[:N]
+
+            # Display the top 5 recommended movies
+            for movie_id, predicted_rating in top_5_recommendations:
+                # Check if the condition results in a non-empty DataFrame
+                if not movies[movies['movieId'] == movie_id].empty:
+                    movie_title = movies[movies['movieId'] == movie_id]['title'].values[0]
+                    rounded_rating = round(predicted_rating, 1)
+                    print(f"Movie: {movie_title}, Predicted Rating: {rounded_rating}")
+                else:
+                    print(f"Movie with ID {movie_id} not found in movies_df")
