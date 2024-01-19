3d57e019adb6dfe6bdb9073b1191a4ceeba5d233
dev-ezzy <bettezra07@gmail.com>
2024-01-19 09:05:00 +0300

    commit

--- recommender3.py
+++ recommender3.py
@@ -44,38 +44,7 @@ def knn_caller(user_id, knn_model):
                     
                 
                 
-# import numpy as np
 
-# def knn_recommender(user_id, knn_model, N):
-#     # Get a list of all movie IDs in your dataset
-#     all_movie_ids = np.unique(modelling_data['movieId'])
-
-#     # Create a list to store predicted ratings for unrated movies
-#     predicted_ratings = []
-
-#     # Predict ratings for the user on unrated movies
-#     for movie_id in all_movie_ids:
-#         #Check if the user has already rated the movie
-#         if not ratings[(ratings['userId'] == user_id) & (ratings['movieId'] == movie_id)].empty:
-#             continue  # Skip if the user has rated the movie
-
-#         predicted_rating = knn_model.predict(user_id, movie_id)
-#         predicted_ratings.append((movie_id, predicted_rating.est))
-
-#         # Sort the predicted ratings in descending order
-#         predicted_ratings.sort(key=lambda x: x[1], reverse=True)
-
-#         #Get the top 5 movie recommendations
-#         top_5_recommendations = predicted_ratings[:N]
-
-#         for movie_id, predicted_rating in top_5_recommendations:
-#             # Check if the condition results in a non-empty DataFrame
-#             if not movies[movies['movieId'] == movie_id].empty:
-#                 movie_title = movies[movies['movieId'] == movie_id]['title'].values[0]
-#                 rounded_rating = round(predicted_rating, 1)
-#                 print(f"Movie: {movie_title}, Predicted Rating: {rounded_rating}")
-#             else:
-#                 print(f"Movie with ID {movie_id} not found in movies_df")
 
 
 def knn_recommender(user_id, knn_model, N):
